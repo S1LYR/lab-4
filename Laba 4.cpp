2d996e89be957c736edfc60d49edf136be7b9cbb
S1LYR <131026211+S1LYR@users.noreply.github.com>
2024-11-14 13:48:13 +0500

    Laba 4.cpp

--- Laba 4.cpp
+++ Laba 4.cpp
@@ -8,8 +8,8 @@ int main() {
     setlocale(LC_ALL, "Russian");
 
     Money m1;  // Создаем объект Money
-    m1.input();  // Вводим данные с клавиатуры
-    m1.display();  // Отображаем введенные данные
+    m1.input();  // Ввод
+    m1.display();  // Вывод
 
     unsigned int kopeksToAdd;
     cout << "Введите количество копеек для добавления: ";
@@ -17,15 +17,15 @@ int main() {
         cin >> kopeksToAdd;
         if (cin.fail() || kopeksToAdd < 0) {
             cout << "Ошибка: введите положительное целое число!" << endl;
-            cin.clear();  // очищаем флаг ошибки
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');  // игнорируем неверный ввод
+            cin.clear(); 
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');  
         }
         else {
             break;
         }
     }
 
-    // Добавляем копейки и выводим результат
+
     cout << "Сумма::" << endl;
 
     m1 = m1 + kopeksToAdd;
@@ -34,38 +34,33 @@ int main() {
     // Унарная операция ++
     cout << "Унарная операция сложения(одна копейка):" << endl;
     ++m1;
-    m1.display();  // После инкремента выводим результат
+    m1.display();  
 
     // Унарная операция --
     cout << "Унарная операция вычитания(одна копейка):" << endl;
 
     --m1;
-    m1.display();  // После декремента выводим результат
+    m1.display();  
 
-    // Ввод для вычитания копеек
     unsigned int kopeksToSubtract;
     cout << "Введите количество копеек для вычитания: ";
     while (true) {
         cin >> kopeksToSubtract;
         if (cin.fail() || kopeksToSubtract < 0) {
             cout << "Ошибка: введите положительное целое число!" << endl;
-            cin.clear();  // очищаем флаг ошибки
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');  // игнорируем неверный ввод
+            cin.clear();  
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');  
         }
         else {
             break;
         }
     }
-
-    // Вычитаем копейки и выводим результат
     m1 = m1 - kopeksToSubtract;
     m1.display();
 
-    // Приведение к unsigned int (только рубли)
     unsigned int rub = static_cast<unsigned int>(m1);
     cout << "Только рубли (unsigned int): " << rub << endl;
 
-    // Приведение к double (копейки в рубли)
     double kopeksInRubles = static_cast<double>(m1);
     cout << "Копейки, переведенные в рубли (double): " << kopeksInRubles << endl;
 
